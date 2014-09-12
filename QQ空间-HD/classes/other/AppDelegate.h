37e166e3603dd70f6766dcb50c47457e337607a7
Xu Lvcai <xulvcai2008@gmail.com>
2014-09-12 22:53:35 +0800

    init

---
+++ QQ空间-HD/classes/other/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QQ空间-HD
+//
+//  Created by apple on 13-9-11.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QQ空间-HD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QQ空间-HD
+//
+//  Created by apple on 13-9-11.
+//  Copyright (c) 2013年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
